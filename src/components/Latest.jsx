28438956f79ca723f78824a7020a19ec69e1de38
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-19 14:25:54 -0700

    add blank scss files for corresponding components.

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import '../scss/latest.scss';
 
 function Latest(){
   return(
-    <p>Latest Section Working</p>
+    <div>
+      <h1>Browse our Latest Collections!</h1>
+      <div className='contain'>
+        <div className='conetentLat'>
+          <div className='imgStandin'></div>
+            <div>
+              <p>EDITOR'S PICKS</p>
+            <h2>Design ideas and inspiration</h2>
+          </div>
+        </div>
+      <div className='conetentLat'>
+        <div className='imgStandin'></div>
+          <div>
+            <p>EDITOR'S PICKS</p>
+            <h2>Jewelry Handbags and accessories</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
