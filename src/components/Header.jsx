c659e13297a01b07d8452bceabce2a942fb8ffdc
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-19 13:39:57 -0700

    add in 'html' for header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 function Header(){
   return(
-    <p>Header Section Working</p>
+    <div>
+      <div className="row">
+        <h1>Etsy</h1>
+        <form className="search">
+          <input type="text" placeholder="Search For Items and Shops"></input>
+          <button type="submit">Search</button>
+        </form>
+        <div className="leftHeader">
+          <p>Sell on Etsy</p>
+          <p>Register</p>
+          <div className="signIn">
+            <p>Sign In</p>
+          </div>
+          <div className="discover">
+          
+            <p>Discover</p>
+          </div>
+          <div className="cart">
+            
+            <p>Cart</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
