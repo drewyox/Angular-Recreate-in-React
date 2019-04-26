ba7e0ba29c006ab915df73d92299843890af8da9
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-26 11:05:16 -0700

    cleaned up extra code while mapping component paths on paper.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -6,39 +6,24 @@ function Review(){
       <div class="containShop">
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>DIY craft projects</p>
-          </div>
         </div>
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>Jewelry gifts under $30</p>
-          </div>
         </div>
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>Vintage clothing</p>
-          </div>
+
         </div>
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>Etsy weddings</p>
-          </div>
+
         </div>
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>One of a kind gift ideas</p>
-          </div>
+
         </div>
         <div class="conetentShop">
           <div class="imgStandinShop"></div>
-          <div>
-            <p>EDITOR'S PICKS</p>
-          </div>
         </div>
       </div>
     </div>
