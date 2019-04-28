5ad9fd68fda23ea2cc1a4eae92a51bf01832143e
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-28 15:59:47 -0700

    add event handler with successfull state change.

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -2,33 +2,35 @@ import React from 'react';
 import '../scss/latest.scss';
 
 class Latest extends React.Component{
-  
+
   constructor(props){
     super(props);
     this.state = {
       displayLatest: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(){
-    console.log('What is up my dude');
+    this.setState({displayLatest: true});
+    console.log('displayLatest is set to:' + this.state.displayLatest);
   }
 
   render(){
     return(
       <div>
-      <p onClick={this.handleClick}>Click Here to See latest items added!</p>
+        <p onClick={this.handleClick}>Click Here to See latest items added!</p>
         <h1>Browse our Latest Collections!</h1>
         <div className='contain'>
           <div className='conetentLat'>
             <div className='imgStandin'></div>
-              <div>
-                <p>EDITOR'S PICKS</p>
+            <div>
+              <p>EDITOR'S PICKS</p>
               <h2>Design ideas and inspiration</h2>
             </div>
           </div>
-        <div className='conetentLat'>
-          <div className='imgStandin'></div>
+          <div className='conetentLat'>
+            <div className='imgStandin'></div>
             <div>
               <p>EDITOR'S PICKS</p>
               <h2>Jewelry Handbags and accessories</h2>
