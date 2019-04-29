78b702e3ed27dbda269a9ff7ce72dbc2722ce4d6
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-28 19:00:06 -0700

    moved content to new component

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import '../scss/latest.scss';
+import LatestContent from './LatestContent'
 
 class Latest extends React.Component{
 
   constructor(props){
     super(props);
     this.state = {
-      displayLatest: false
+      // displayLatest: false
     };
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-    this.setState({displayLatest: true});
-    console.log('displayLatest is set to:' + this.state.displayLatest);
-  }
+  // handleClick(){
+  //   this.setState({displayLatest: true});
+  //   console.log('displayLatest is set to:' + this.state.displayLatest);
+  // }
 
   render(){
     return(
       <div>
         <p onClick={this.handleClick}>Click Here to See latest items added!</p>
-        <h1>Browse our Latest Collections!</h1>
-        <div className='contain'>
-          <div className='conetentLat'>
-            <div className='imgStandin'></div>
-            <div>
-              <p>EDITOR'S PICKS</p>
-              <h2>Design ideas and inspiration</h2>
-            </div>
-          </div>
-          <div className='conetentLat'>
-            <div className='imgStandin'></div>
-            <div>
-              <p>EDITOR'S PICKS</p>
-              <h2>Jewelry Handbags and accessories</h2>
-            </div>
-          </div>
-        </div>
+        <LatestContent/>
       </div>
     );
   }
---
+++ src/components/LatestContent.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function LatestContent(){
+  return (
+    <div>
+      <h1>Browse our Latest Collections!</h1>
+      <div className='contain'>
+        <div className='conetentLat'>
+          <div className='imgStandin'></div>
+          <div>
+            <p>EDITOR'S PICKS</p>
+            <h2>Design ideas and inspiration</h2>
+          </div>
+        </div>
+        <div className='conetentLat'>
+          <div className='imgStandin'></div>
+          <div>
+            <p>EDITOR'S PICKS</p>
+            <h2>Jewelry Handbags and accessories</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default LatestContent
